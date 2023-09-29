71fff1ffde4a157931760bdd787ae8ae4478a233
GodPepe7 <privatalex@gmail.com>
2023-09-29 17:47:59 +0700

    Implement ship factory

---
+++ src/ship.js
@@ -0,0 +1,12 @@
+const ship = (len) => {
+  let hits = 0;
+  const length = len;
+  const getHits = () => hits;
+  const hit = () => {
+    hits += 1;
+  };
+  const isSunk = () => length - hits <= 0;
+  return { length, hit, isSunk, getHits };
+};
+
+export default ship;
