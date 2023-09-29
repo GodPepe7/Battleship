d24fad62305cbe089b83079b08c3082bdb743ace
GodPepe7 <privatalex@gmail.com>
2023-09-29 17:46:56 +0700

    Write ship tests

---
+++ src/ship.test.js
@@ -0,0 +1,26 @@
+import ship from "./ship";
+
+describe("ship tests", () => {
+  let newShip;
+  beforeEach(() => {
+    newShip = ship(3);
+  });
+
+  test("ship length", () => {
+    expect(newShip.length).toBe(3);
+  });
+
+  test("ship hit", () => {
+    expect(newShip.getHits()).toBe(0);
+    newShip.hit();
+    expect(newShip.getHits()).toBe(1);
+  });
+
+  test("ship sunk", () => {
+    expect(newShip.isSunk()).toBe(false);
+    newShip.hit();
+    newShip.hit();
+    newShip.hit();
+    expect(newShip.isSunk()).toBe(true);
+  });
+});
