c4f6d81da3ef56906ee0a51d211c6c0318747feb
GodPepe7 <privatalex@gmail.com>
2023-09-29 17:48:57 +0700

    Implement gameboard factory

---
+++ src/gameboard.js
@@ -0,0 +1,38 @@
+const gameboard = () => {
+  const board = [];
+  const attackedFields = [];
+  const ships = [];
+  for (let i = 0; i < 10; i++) {
+    const row = new Array(10);
+    board.push(row);
+  }
+  const getBoard = () => board;
+  const getAttackedFields = () => attackedFields;
+  const placeShip = (ship, x, y) => {
+    ships.push(ship);
+    board[y][x] = ship;
+  };
+  const receiveAttack = (x, y) => {
+    const attacked = board[y][x];
+    if (!attacked) board[y][x] = attackedFields.push({ hit: false, x, y });
+    else {
+      attacked.hit();
+      attackedFields.push({ hit: true, x, y });
+    }
+  };
+  const allShipsSunk = () => {
+    ships.forEach((ship) => {
+      if (!ship.isSunk()) return false;
+    });
+    return true;
+  };
+  return {
+    getBoard,
+    getAttackedFields,
+    placeShip,
+    receiveAttack,
+    allShipsSunk,
+  };
+};
+
+export default gameboard;
