5f3124ed9fdcdba3aa6409dc083213a7043fdcf9
GodPepe7 <privatalexm@gmail.com>
2023-10-18 15:54:23 +0200

    Implement player and computer attack

--- src/model/Player.js
+++ src/model/Player.js
@@ -1,15 +1,33 @@
 import Ship from "./Ship";
-import Gameboard from "./Gameboard";
+import GameBoard from "./GameBoard";
 
 const Player = () => {
+  const gameBoard = GameBoard();
   const ships = [];
-  const gameboard = Gameboard();
   ships.push(Ship(5));
   ships.push(Ship(4));
   ships.push(Ship(3));
   ships.push(Ship(3));
   ships.push(Ship(2));
-  return { ships, gameboard };
+
+  const attackEnemy = (x, y, enemyGameBoard) => {
+    enemyGameBoard.receiveAttack(x, y);
+  };
+
+  const computerAttackEnemy = (enemyGameBoard) => {
+    const attackedFields = enemyGameBoard.getAttackedFields();
+    const notAttacked = [];
+    for (let i = 0; i < 100; i++) {
+      if (!attackedFields.has(i)) notAttacked.push(i);
+    }
+    const randomIndex = Math.floor(Math.random() * notAttacked.length);
+    const attackCoordinate = notAttacked[randomIndex];
+    const attackY = Math.floor(attackCoordinate / 10);
+    const attackX = attackCoordinate - attackY * 10;
+    enemyGameBoard.receiveAttack(attackX, attackY);
+  };
+
+  return { gameBoard, ships, attackEnemy, computerAttackEnemy };
 };
 
 export default Player;
