02cc8f2ab8bc5dfabea285270aecf2fa9c1c6677
GodPepe7 <privatalexm@gmail.com>
2023-10-22 12:25:39 +0200

    Rewrite placeShip and implement other functions

--- src/model/GameBoard.js
+++ src/model/GameBoard.js
@@ -1,19 +1,45 @@
 const GameBoard = () => {
+  const BOARD_SIZE = 10;
   const board = [];
   const attackedFields = new Map();
   const ships = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < BOARD_SIZE; i++) {
     const row = new Array(10);
     board.push(row);
   }
 
   const getBoard = () => board;
   const getAttackedFields = () => attackedFields;
-  const placeShip = (ship, coordinates) => {
+  const checkIfValidPlacement = (shipLength, [x, y], axis) => {
+    if (axis === "x") {
+      //checks if coordinate is in bounds of 10x10 board
+      if (board[y].length - 1 < x + shipLength - 1) return false;
+      for (let i = 0; i < shipLength; i++) {
+        //checks if cell is already occupied by other ship
+        if (board[y][x + i]) return false;
+      }
+      return true;
+    } else {
+      if (board.length - 1 < y + shipLength - 1) return false;
+      for (let i = 0; i < shipLength; i++) {
+        if (board[y + i][x]) return false;
+      }
+      return true;
+    }
+  };
+
+  const placeShip = (ship, [x, y], axis) => {
+    if (!checkIfValidPlacement(ship.length, [x, y], axis)) return;
     ships.push(ship);
-    coordinates.forEach(([x, y]) => {
-      board[y][x] = ship;
-    });
+    if (axis === "x") {
+      for (let i = 0; i < ship.length; i++) {
+        board[y][x + i] = ship;
+      }
+    } else {
+      for (let i = 0; i < ship.length; i++) {
+        board[y + i][x] = ship;
+      }
+    }
   };
 
   const receiveAttack = (x, y) => {
@@ -31,12 +57,33 @@ const GameBoard = () => {
     return ships.every((ship) => ship.isSunk());
   };
 
+  const generateRandomCoordinates = (shipLength, axis) => {
+    const availableCells = [];
+    for (let y = 0; y < BOARD_SIZE; y++) {
+      for (let x = 0; x < BOARD_SIZE; x++) {
+        if (checkIfValidPlacement(shipLength, [x, y], axis))
+          availableCells.push([x, y]);
+      }
+    }
+    const randomCoordinate =
+      availableCells[Math.floor(Math.random() * availableCells.length - 1)];
+    return randomCoordinate;
+  };
+
+  const placeShipAtRandomCoordinate = (ship) => {
+    let axis = Math.random() > 0.5 ? "x" : "y";
+    const randomCoordinate = generateRandomCoordinates(ship.length, axis);
+    placeShip(ship, randomCoordinate, axis);
+  };
+
   return {
     getBoard,
+    checkIfValidPlacement,
     getAttackedFields,
     placeShip,
     receiveAttack,
     allShipsSunk,
+    placeShipAtRandomCoordinate,
   };
 };
 
